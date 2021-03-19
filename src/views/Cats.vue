e614241511e9822177ce4c6edbb809a157f17b7f
Fikremariam <fikrea197@gmail.com>
2021-03-19 12:47:24 +0300

    initial

---
+++ src/views/Cats.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Cats for Adoption</h1>
+    <b-table stripped hover :items="cats">
+      <template #cell(name)="data">
+        <router-link :to="`/pet/cats/${data.index}`">{{ data.value }}</router-link>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
+  }
+}
+</script>
---
+++ src/views/Dogs.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    <h1>Dogs for Adoption</h1>
+    <b-table stripped hover :items="dogs">
+      <template #cell(name)="data">
+        <router-link :to="`/pet/dogs/${data.index}`">{{ data.value }}</router-link>
+      </template>
+    </b-table>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'dogs'
+    ])
+  }
+}
+</script>
